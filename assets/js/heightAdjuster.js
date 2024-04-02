55738dffa72b83cf9ed2118079bffbaf56065ff1
ManasTom <manastom670@gmail.com>
2024-04-02 12:15:05 +0530

    anjali about sec done

---
+++ assets/js/heightAdjuster.js
@@ -0,0 +1,19 @@
+// const firstDiva = document.querySelector('.services_page_section4_colItem1');
+// const secondDiva = document.querySelector('.services_page_section4_colItem2');
+// const thirdDiva = document.querySelector('.services_page_section4_colItem3');
+// secondDiva.style.height = `${firstDiva.clientHeight}px`;
+// thirdDiva.style.height = `${firstDiva.clientHeight}px`;
+
+
+const vhs1 = document.querySelector('.hs1');
+const vhs2 = document.querySelector('.hs2');
+const vhs3 = document.querySelector('.hs3');
+const vhs4 = document.querySelector('.hs4');
+const vhs5 = document.querySelector('.hs5');
+const vhs6 = document.querySelector('.hs6');
+
+vhs1.style.height = `${vhs5.clientHeight}px`;
+vhs2.style.height = `${vhs5.clientHeight}px`;
+vhs3.style.height = `${vhs5.clientHeight}px`;
+vhs4.style.height = `${vhs5.clientHeight}px`;
+vhs6.style.height = `${vhs5.clientHeight}px`;
