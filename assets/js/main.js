f901aa82b3cfc9c3bc03165bdb37927399754de3
ManasTom <manastom670@gmail.com>
2024-07-05 12:47:07 +0530

    UPDATES

--- assets/js/main.js
+++ assets/js/main.js
@@ -1678,3 +1678,95 @@ function showApprovedReviews() {
 
 
 
+
+
+
+
+
+
+
+
+
+// ********************************************************************************************
+// Function to logout user after 1 hour of inactivity since last user activity
+// ********************************************************************************************
+
+window.onload = function () {
+    // Function to handle user logout
+    function logoutUser() {
+        console.log("Logging out user due to inactivity or tab/window close");
+        auth.signOut().then(() => {
+            // Clear any user session data
+            localStorage.clear();
+            sessionStorage.clear();
+
+            // Redirect to login page after logout
+            window.location.href = 'index.html';
+        });
+    }
+
+    // Function to set login time
+    function setLoginTime() {
+        var loginTime = new Date().getTime();
+        localStorage.setItem('loginTime', loginTime);
+        console.log("Login time set at: " + new Date(loginTime));
+    }
+
+    // Function to check inactivity based on login time
+    function checkInactivity() {
+        var loginTime = localStorage.getItem('loginTime');
+        if (loginTime) {
+            var currentTime = new Date().getTime();
+            var timeSinceLogin = currentTime - loginTime;
+
+            if (timeSinceLogin > 60 * 60 * 1000) { // 1 hour
+                logoutUser();
+            } else {
+                // Reset the inactivity timeout to check again after the remaining time
+                var remainingTime = 60 * 60 * 1000 - timeSinceLogin;
+                clearTimeout(inactivityTimeout);
+                inactivityTimeout = setTimeout(logoutUser, remainingTime);
+            }
+        }
+    }
+
+    // Set login time on load if not already set
+    if (!localStorage.getItem('loginTime')) {
+        setLoginTime();
+    }
+
+    // Check inactivity on page load/reload
+    checkInactivity();
+
+    // Event listeners for user activity to reset login time
+    document.addEventListener("mousemove", function () {
+        setLoginTime();
+        checkInactivity();
+    });
+
+    document.addEventListener("keypress", function () {
+        setLoginTime();
+        checkInactivity();
+    });
+
+    // Variable to store the inactivity timeout
+    var inactivityTimeout;
+
+    // Log out the user when closing the tab or browser
+    window.addEventListener("beforeunload", function () {
+        logoutUser();
+    });
+
+
+    window.addEventListener('unload', function () {
+        logoutUser();
+    });
+
+};
+
+
+
+
+
+
+
